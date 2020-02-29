0470e335398ba8954e97086cbb381943dc309e95
Brandon <brandonl271@gmail.com>
2020-02-29 12:17:24 -0800

    j

--- src/index.js
+++ src/index.js
@@ -1,16 +1,10 @@
 
 let httpRequest= new XMLHttpRequest();
 console.log(httpRequest);
-httpRequest.onreadystatechange= function(){
-                if(httpRequest.readyState === 4){
-                    if(httpRequest.status === 200){
-                        console.log(httpRequest.status)
-                        console.log(httpRequest.responseText);
-                    }
-                }
-        }
-httpRequest.open("GET", "https://blockchain.info/ticker");
+httpRequest.onreadystatechange= requestHandler;
 
+httpRequest.open("GET", "https://blockchain.info/ticker");
+httpRequest.send();
 let requestHandler= function(){
                 if(httpRequest.readyState === 4){
                     if(httpRequest.status === 200){

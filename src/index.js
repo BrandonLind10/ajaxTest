6e239f6823082fea052e9e89f7fca933e7d353a6
Brandon <brandonl271@gmail.com>
2020-02-29 12:19:44 -0800

    g

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ httpRequest.onreadystatechange= requestHandler;
 
 httpRequest.open("GET", "https://blockchain.info/ticker");
 httpRequest.send();
+console.log("sent")
 let requestHandler= function(){
                 if(httpRequest.readyState === 4){
                     if(httpRequest.status === 200){
